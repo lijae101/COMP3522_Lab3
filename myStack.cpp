10c4c66e92ea9d1d687e50dd524b9fbf23628f73
Liam Janicke <liam.jaenicke@gmail.com>
2024-09-20 16:41:26 -0700

    Added some comments

--- myStack.cpp
+++ myStack.cpp
@@ -10,6 +10,7 @@
 // Constructor to initialize the stack
 MyStack::MyStack() : topPointer(-1) {}
 
+//Function to push values onto the stack
 bool MyStack::push(int value) {
     if(full()) {
         //return false if the stack is full
@@ -20,12 +21,14 @@ bool MyStack::push(int value) {
     return true;
 }
 
+//Function to pop elements from the stack
 void MyStack::pop() {
     if(!empty()) {
         --topPointer;
     }
 }
 
+//Function to check the current top value of the stack
 int MyStack::top() const {
     if(empty()) {
         return EMPTY_STACK_VALUE;
@@ -34,14 +37,17 @@ int MyStack::top() const {
 
 }
 
+//Function to check whether the stack is empty
 bool MyStack::empty() const {
     return topPointer == -1;
 }
 
+//function to check whether the stack is full
 bool MyStack::full() const {
     return topPointer == MAX_SIZE -1;
 }
 
+//Function to print the stack
 string MyStack::print() const {
     stringstream ss;
     if (empty()) {
